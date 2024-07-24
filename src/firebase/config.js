d0ddb848ae17e5c20b2c7d71b8209b024babefe3
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-07-24 17:11:29 +0100

    update config for firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore } from 'firebase/firestore';
-// import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,11 +19,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const db = getFirestore(app);
-// const auth = getAuth(app);
-
-// export {db,auth};
-
-export default firebaseApp;
+export const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
